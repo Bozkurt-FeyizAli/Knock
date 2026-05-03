020b3a0b7d5d3c5aa7114f41114cc99d8dec0268
Bozkurt-FeyizAli <Feyizalibozkurt@gmail.com>
2026-05-03 12:13:17 +0300

    fe: enviroment files

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost:5264/api' // Adjust to the actual port your .NET backend runs on
+  apiUrl: 'https://feyizalibozkurt.dev/api' // Adjust to the actual port your .NET backend runs on
+  // apiUrl: 'http://localhost:5264/api'  if work in local machine
 };
